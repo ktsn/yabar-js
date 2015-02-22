69bc968c6b88c3fd19169a0bd5cd0cb1af5ec78f
katashin <ktsn55@gmail.com>
2015-02-23 00:09:39 +0900

    use sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var del = require('del');
 var gulp = require('gulp');
 var coffee = require('gulp-coffee');
+var sass = require('gulp-sass');
 var uglify = require('gulp-uglifyjs');
 
 gulp.task('clean', function(cb) {
@@ -13,10 +14,18 @@ gulp.task('compress', ['build'], function() {
     .pipe(gulp.dest('build/min/'));
 });
 
-gulp.task('build', ['clean'], function() {
+gulp.task('sass', ['clean'], function() {
+  return gulp.src(['sass/**/*.sass'])
+    .pipe(sass({ indentedSyntax: true }))
+    .pipe(gulp.dest('build/css/'));
+});
+
+gulp.task('coffee', ['clean'], function() {
   return gulp.src(['src/**/*.coffee'])
     .pipe(coffee().on('error', function(err) { throw err }))
     .pipe(gulp.dest('build/js/'));
 });
+
+gulp.task('build', ['sass', 'coffee']);
 
 gulp.task('default', ['compress']);
